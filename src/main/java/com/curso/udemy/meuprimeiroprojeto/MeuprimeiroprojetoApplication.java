c3c8066e5e52587ce59bf93e05851eece37d0b03
RodPSouza <rod.p.souza@hotmail.com>
2020-03-03 19:34:15 -0300

    Alteração do arquivo principal da aplicação

--- src/main/java/com/curso/udemy/meuprimeiroprojeto/MeuprimeiroprojetoApplication.java
+++ src/main/java/com/curso/udemy/meuprimeiroprojeto/MeuprimeiroprojetoApplication.java
@@ -8,6 +8,7 @@ public class MeuprimeiroprojetoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MeuprimeiroprojetoApplication.class, args);
+		System.out.println("Meu primeiro projeto...");
 	}
 
 }
